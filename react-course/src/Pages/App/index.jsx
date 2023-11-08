b44f58118e31548aab33711546d5e8dd5835b7da
David Sanchez <davidss2995@gmail.com>
2023-11-07 23:31:47 -0600

    Including React Router
    Including React Router

--- react-course/src/Pages/App/index.jsx
+++ react-course/src/Pages/App/index.jsx
@@ -1,27 +1,33 @@
-import Home from '../Home'
-import MyAccount from '../MyAccount'
-import MyOrder from '../MyOrder'
-import MyOrders from '../MyOrders'
-import NotFound from '../NotFound'
-import SignIn from '../SignIn'
+import { useRoutes, BrowserRouter } from "react-router-dom";
+import Home from "../Home";
+import MyAccount from "../MyAccount";
+import MyOrder from "../MyOrder";
+import MyOrders from "../MyOrders";
+import NotFound from "../NotFound";
+import SignIn from "../SignIn";
+import Navbar from "../../Components/Navbar";
+import "./App.css";
 
-import './App.css'
+const AppRoutes = () => {
+  let routes = useRoutes([
+    { path: "/", element: <Home /> },
+    { path: "/my-account", element: <MyAccount /> },
+    { path: "/my-order", element: <MyOrder /> },
+    { path: "/my-orders", element: <MyOrders /> },
+    { path: "/sign-in", element: <SignIn /> },
+    { path: "/*", element: <NotFound /> },
+  ]);
 
-function App() {
-  
+  return routes;
+};
 
+const App = () => {
   return (
-    <>
-      <div className="bg-red-100">
-        <Home />
-        <MyAccount />
-        <MyOrder />
-        <MyOrders />
-        <NotFound />
-        <SignIn />
-      </div>      
-    </>
-  )
-}
+    <BrowserRouter>
+      <AppRoutes></AppRoutes>
+      <Navbar></Navbar>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
